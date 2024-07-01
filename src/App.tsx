191adacdd909b692fc65e771d22087de85eec456
Nazmos27 <nazmos789@gmail.com>
2024-07-01 17:50:38 +0600

    project setup with shadcdn UI,tailwind and tsc

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1> To do app with redux</h1>
+      <Button>This is shadcdn button</Button>
+    </>
+  );
+}
+
+export default App;
